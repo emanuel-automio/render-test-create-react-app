0d1b7e2382f760f522eb1d81c413eeef12607369
ema <ema@emas-MacBook-Pro.local>
2022-05-03 09:13:23 +1200

    M src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    fetch('https://express-rest-api.onrender.com/users')
+    .then(response => response.json())
+    .then(response => setResult(response))
+    .catch(error => setResult(error));
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        result.map( r => (
+            //@ts-ignore  
+            <span key={r.id}>{r.id}: {r.name}</span>
+        ))
+      }
     </div>
   );
 }
